18a49f2f50dda084495b211401a142747011912a
Arthur Fincham <arthurfincham@me.com>
2021-11-30 15:25:41 +0000

    add sampleData, call it in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import NewsCard from './components/NewsCard';
 
 function App() {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch('data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data &&
+        data.length > 0 &&
+        data.map((item) => (
+          <NewsCard
+            key={item.id}
+            publisher={item.publisher}
+            publish_date={item.publish_date}
+            image_url={item.image_url}
+            headline={item.headline}
+            sub_headline={item.sub_headline}
+            article_body={item.article_body}
+          />
+        ))}
     </div>
   );
 }
