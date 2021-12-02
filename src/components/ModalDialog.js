0afdf14fade200c4dff6e8b3eb7ab2a115afc9c0
Alex Boyling <alexboyling@gmail.com>
2021-12-02 11:21:19 +0000

    Created ModalDialog.js

---
+++ src/components/ModalDialog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+// import Form from './Form';
+
+const ModalDialog = ({ open, handleClose }) => {
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      // form to create
+      // <Form handleClose={handleClose} />
+    </Dialog>
+  )
+}
