39503dbf5cdb5353f45a1c25e71b49db51b531f0
Arthur Fincham <arthurfincham@me.com>
2021-12-06 10:50:35 +0000

    move css into index.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./components/globalStyles";
-import { lightTheme, darkTheme } from "./components/Themes";
-import Navigation from "./components/Navigation";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import AboutUs from "./components/AboutUs";
-import SignUp from "./components/SignUp";
-import LogIn from "./components/LogIn";
-
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './components/globalStyles';
+import { lightTheme, darkTheme } from './components/Themes';
+import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import SignUp from './components/SignUp';
+import LogIn from './components/LogIn';
+import './App.css';
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState('light');
   const themeToggler = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
+    theme === 'light' ? setTheme('dark') : setTheme('light');
   };
 
   return (
     <Router>
       <>
-        <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
           <Navigation themeToggler={themeToggler} />
 
           <>
