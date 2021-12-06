ce77fe56a2a0c2b8a1a05941a892d9415997352f
Alex Boyling <alexboyling@gmail.com>
2021-12-06 16:13:47 +0000

    log in form tests

--- src/components/SignUp.test.js
+++ src/components/SignUp.test.js
@@ -1,22 +1,8 @@
-// import React from 'react';
-// import { render, fireEvent, screen } from '@testing-library/react';
-// import SignUp from './SignUp';
-
-// it('can be dismissed', () => {
-//   const handleClose =jest.fn();
-//   const { getByText } = render(<SignUp handleClose={handleClose} />);
-//   fireEvent.click(getByText("Cancel"));
-//   expect(handleClose).toHaveBeenCalled();
-// });
-
 import SignUp from "./SignUp";
 import React from "react";
 import ReactDOM from "react-dom";
 import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import TextField from '@mui/material/TextField';
-
-afterEach(cleanup);
 
 it('renders without crashing', () => {
   render(<SignUp />)
