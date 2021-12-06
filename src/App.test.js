4ee74691094877554bbd3b3fa8b8efc004b6dced
hamstercat007 <lisamac421@gmail.com>
2021-12-06 10:59:22 +0000

    Tested for content on About Us page in feature test; test moved from App.js to About Us

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import App from "./App";
 
 afterEach(cleanup);
@@ -7,12 +7,4 @@ test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/newsforce/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-test("should navigate to About Us page when link is clicked", () => {
-  const { getByText } = render(<App></App>);
-  expect(window.location.href).toBe("http://localhost/");
-  const link = getByText("About Us");
-  fireEvent.click(link);
-  expect(window.location.href).toBe("http://localhost/about");
 });
--- src/components/AboutUs/AboutUs.test.js
+++ src/components/AboutUs/AboutUs.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AboutUs from "./AboutUs";
-import { render, cleanup } from "@testing-library/react";
+import { fireEvent, render, screen, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import App from "../../App";
 
 afterEach(cleanup);
 
@@ -16,4 +17,12 @@ it("renders text correctly", () => {
   expect(getByTestId("aboutUsTitle")).toHaveTextContent("A Force for Good");
 });
 
-
+test("should navigate to About Us page when link is clicked", () => {
+  const { getByText } = render(<App></App>);
+  expect(window.location.href).toBe("http://localhost/");
+  const link = getByText("About Us");
+  fireEvent.click(link);
+  expect(window.location.href).toBe("http://localhost/about");
+  const headerElement = screen.getByText(/A Force For Good/i);
+  expect(headerElement).toBeInTheDocument();
+});
