e9aca247aa0468ef7a4a04ff4e27463963b6f215
hamstercat007 <lisamac421@gmail.com>
2021-12-07 16:13:41 +0000

    lazyloading package added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import NewsCard from './NewsCard';
-import Grid from '@mui/material/Grid';
-import { useState, useEffect } from 'react';
-import SkeletonCard from './SkeletonCard';
-import LazyLoad from 'react-lazyload';
-
-
+import React from "react";
+import NewsCard from "./NewsCard";
+import Grid from "@mui/material/Grid";
+import { useState, useEffect } from "react";
+import SkeletonCard from "./SkeletonCard";
+import LazyLoad from "react-lazyload";
 
 const Home = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const skelArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   const getData = () => {
-    fetch('https://newsforce-api.herokuapp.com/index', {
+    fetch("https://newsforce-api.herokuapp.com/index", {
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
     })
       .then(function (response) {
@@ -54,17 +52,19 @@ const Home = () => {
             data.length > 0 &&
             data.map((item) => (
               <Grid item key={item.id} xs={12} sm={6} md={5}>
-                <NewsCard
-                  key={item.id}
-                  publisher={item.publisher}
-                  publish_date={item.publish_date}
-                  image_url={item.image_url}
-                  headline={item.headline}
-                  sub_headline={item.sub_headline}
-                  article_body={item.article_body}
-                  src_url={item.source_url}
-                  tag_list={item.tag_list}
-                />
+                <LazyLoad height={200}>
+                  <NewsCard
+                    key={item.id}
+                    publisher={item.publisher}
+                    publish_date={item.publish_date}
+                    image_url={item.image_url}
+                    headline={item.headline}
+                    sub_headline={item.sub_headline}
+                    article_body={item.article_body}
+                    src_url={item.source_url}
+                    tag_list={item.tag_list}
+                  />
+                </LazyLoad>
               </Grid>
             ))}
         </Grid>
