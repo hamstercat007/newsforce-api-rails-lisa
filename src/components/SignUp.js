d21e95e1824a64d31a0a692623c2b0b8bee132e6
Alex Boyling <alexboyling@gmail.com>
2021-12-07 13:46:40 +0000

    Changed sign-up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,23 +6,16 @@ import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import {useNavigate} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const SignUp = () => {
-  const navigate = useNavigate();
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
-    // Update this with API call to user db
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
 
-    const creds = { "session": {"email": data.get('email'), "password": data.get('password') }};
+    const creds = { "user": {"email": data.get('email'), "password": data.get('password') }};
 
-    fetch('https://localhost:3000/api/signup', {
+    fetch('https://newsforce-api.herokuapp.com/api/signup', {
       method: 'POST',
       credentials: 'include',
       headers: {
@@ -32,7 +25,7 @@ const SignUp = () => {
     }).then(response => response.json())
     .then(data => {
       console.log('Success:', data);
-      navigate('/')
+      <Navigate to="/" replace />
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -63,25 +56,6 @@ const SignUp = () => {
               Sign Up
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              autoComplete="given-name"
-              name="firstName"
-              required
-              fullWidth
-              id="firstName"
-              label="First Name"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="lastName"
-              label="Last Name"
-              name="lastName"
-              autoComplete="family-name"
-            />
             <TextField
               margin="normal"
               required
