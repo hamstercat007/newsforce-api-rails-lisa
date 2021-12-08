e6f2930fc546b70c0a6a681ebd1fdb1afe819a2a
hamstercat007 <lisamac421@gmail.com>
2021-12-08 15:29:11 +0000

    cleaning up unused components

--- src/components/TeamBio.js
+++ src/components/TeamBio.js
@@ -6,11 +6,9 @@ import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { blue } from "@mui/material/colors";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReact, faGithub } from "@fortawesome/free-brands-svg-icons";
 import List from "@mui/material/List";
@@ -19,16 +17,6 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
 
 export default function TeamBio({
   name,
@@ -39,12 +27,7 @@ export default function TeamBio({
   strength2,
   strength3,
 }) {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+git
   return (
     <Card sx={{ minWidth: 225 }} className="enlargeOnHover" raised="true">
       <CardHeader title={name} style={{ fontSize: "2em" }} align="center" />
