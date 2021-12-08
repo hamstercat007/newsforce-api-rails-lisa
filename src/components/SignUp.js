932f269dca963e795456d56c1bebe8f4306c9897
Alex Boyling <alexboyling@gmail.com>
2021-12-08 10:25:13 +0000

    Fixed signup error

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -83,23 +83,13 @@ const SignUp = () => {
               sx={{ mt: 3, mb: 2 }}
             >
               Sign Up
-            </Typography>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-              <TextField margin="normal" autoComplete="given-name" name="firstName" required fullWidth id="firstName" label="First Name" autoFocus />
-              <TextField margin="normal" required fullWidth id="lastName" label="Last Name" name="lastName" autoComplete="family-name" />
-              <TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" />
-              <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" />
-              <TextField margin="normal" required fullWidth name="confirmPassword" label="Confirm Password" type="password" id="confirmPassword" />
-              <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-                Sign Up
-              </Button>
-              <Link to="/login" variant="body2">
+            </Button>
+            <Link to="/login" variant="body2">
                 {'Already have an account? Log in'}
-              </Link>
-            </Box>
+            </Link>
           </Box>
         </Card>
-      </Container>
+      </Container>        
     </>
   );
 };
